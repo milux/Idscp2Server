478caa1943d1bf63f1eae42d1b2ca54fe115e277
Michael Lux <michael.lux@aisec.fraunhofer.de>
2021-04-20 09:26:40 +0200

    Fixed generics and listeners

--- src/main/java/idscp2server/IDSServer.java
+++ src/main/java/idscp2server/IDSServer.java
@@ -7,34 +7,19 @@ import de.fhg.aisec.ids.idscp2.default_drivers.rat.dummy.RatProverDummy;
 import de.fhg.aisec.ids.idscp2.default_drivers.rat.dummy.RatVerifierDummy;
 import de.fhg.aisec.ids.idscp2.default_drivers.secure_channel.tlsv1_3.NativeTLSDriver;
 import de.fhg.aisec.ids.idscp2.default_drivers.secure_channel.tlsv1_3.NativeTlsConfiguration;
+import de.fhg.aisec.ids.idscp2.idscp_core.api.Idscp2EndpointListener;
 import de.fhg.aisec.ids.idscp2.idscp_core.api.configuration.AttestationConfig;
 import de.fhg.aisec.ids.idscp2.idscp_core.api.configuration.Idscp2Configuration;
-import de.fhg.aisec.ids.idscp2.idscp_core.api.idscp_connection.Idscp2Connection;
-import de.fhg.aisec.ids.idscp2.idscp_core.api.idscp_connection.Idscp2ConnectionAdapter;
-import de.fhg.aisec.ids.idscp2.idscp_core.api.idscp_connection.Idscp2ConnectionImpl;
-import de.fhg.aisec.ids.idscp2.idscp_core.api.idscp_connection.Idscp2ConnectionListener;
-import de.fhg.aisec.ids.idscp2.idscp_core.api.idscp_connection.Idscp2MessageListener;
 import de.fhg.aisec.ids.idscp2.idscp_core.api.idscp_server.Idscp2Server;
 import de.fhg.aisec.ids.idscp2.idscp_core.api.idscp_server.Idscp2ServerFactory;
 import de.fhg.aisec.ids.idscp2.idscp_core.drivers.SecureChannelDriver;
-import de.fhg.aisec.ids.idscp2.idscp_core.error.Idscp2Exception;
-import de.fhg.aisec.ids.idscp2.idscp_core.error.Idscp2NotConnectedException;
-import de.fhg.aisec.ids.idscp2.idscp_core.error.Idscp2TimeoutException;
-import de.fhg.aisec.ids.idscp2.idscp_core.error.Idscp2WouldBlockException;
 import de.fhg.aisec.ids.idscp2.idscp_core.rat_registry.RatProverDriverRegistry;
 import de.fhg.aisec.ids.idscp2.idscp_core.rat_registry.RatVerifierDriverRegistry;
-
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Paths;
 
 
 /**
@@ -111,9 +96,11 @@ public class IDSServer{
                     .build();
 
             SecureChannelDriver<AppLayerConnection, NativeTlsConfiguration> secureChannelDriver = new NativeTLSDriver<>();
-            Idscp2EndpointListenerImpl listImpl = new Idscp2EndpointListenerImpl();
 
-            Idscp2ServerFactory serverFactory = new Idscp2ServerFactory(AppLayerConnection::new,listImpl,serverConfiguration, secureChannelDriver, secureChannelConfig);
+            Idscp2ServerFactory<AppLayerConnection, NativeTlsConfiguration> serverFactory =
+                    new Idscp2ServerFactory<>(AppLayerConnection::new, appLayerConnection -> {
+                        LOG.debug("New server connection: {}", appLayerConnection);
+                    }, serverConfiguration, secureChannelDriver, secureChannelConfig);
                   
             Idscp2Server server = serverFactory.listen();
  
--- src/main/java/idscp2server/Idscp2EndpointListener.java
+++
@@ -1,8 +0,0 @@
-package idscp2server;
-
-import de.fhg.aisec.ids.idscp2.app_layer.AppLayerConnection;
-
-public interface Idscp2EndpointListener{
-
-	public void onConnection( AppLayerConnection connection );
-}
